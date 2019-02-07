b765b359c01d7b9a2d69180edae2d0ae5094be24
Peerapong Thammakaew (Golf) <golf.prptmk@gmail.com>
2019-02-08 05:37:15 +0700

    พยายามทำปุ่ม read more

---
+++ src/components/newsrow.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="row block">
+    <div class="col-md-9">
+      <div class="title">{{title}}</div>
+      <div class="des">{{description}}</div>
+      <golfbtn text="btntxt" />
+      <div class="golfbtn">Read More</div>
+    </div>
+    <div class="col-md-3">
+      <img
+        :src="img"
+        style="width: 100%; overflow: hidden; border-radius: 5px;"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import golfbtn from '@/components/golfbotton'
+export default {
+  name: 'newsrow',
+  props: ['title', 'description', 'img'],
+  components: {
+    golfbtn
+  }
+}
+</script>
+
+<style scoped>
+.golfbtn {
+  background-color: #0ca514;
+  color: #fff !important;
+    -webkit-box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
+    box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
+    padding: 0.84rem 2.14rem;
+    font-size: 0.81rem;
+    -webkit-transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out, border-color 0.15s ease-in-out, -webkit-box-shadow 0.15s ease-in-out;
+    transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out, border-color 0.15s ease-in-out, -webkit-box-shadow 0.15s ease-in-out;
+    transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out, border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+    transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out, border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out, -webkit-box-shadow 0.15s ease-in-out;
+    margin: 0.375rem;
+    border: 0;
+    border-radius: 5px;
+    cursor: pointer;
+    text-transform: uppercase;
+    white-space: normal;
+    word-wrap: break-word;
+}
+.title {
+  margin: 10px 0px 10px 0px;
+  padding-left: 10px;
+  color: #e3b800;
+  font-weight: bold;
+  font-size: 20px;
+  border-color: #e3b800;
+  border: solid;
+  border-width: 0px 0px 0px 3px;
+}
+.des {
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+.block{
+  padding: 30px;
+  border: solid;
+  border-width: 0px 0px 1px 0px;
+  border-color: #f8f9fa;
+  background-color: white;
+}
+.block:hover{
+  transition-duration: 0.2s;
+  background-color: rgb(250, 250, 250);
+}
+</style>
